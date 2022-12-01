cbf34a667637e4a808092546eeeef3e6e1800ec6
Илья Соболев <i4sobolev@yandex.ru>
2022-12-02 00:48:37 +0300

    user added

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    """Поля видимости в админке"""
+
+    list_display = (
+        'pk',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(User, UserAdmin)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Пользовательский класс"""
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя'
+    )
+
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия'
+    )
+
+    username = models.CharField(
+        max_length=150,
+        verbose_name='Уникальный юзернейм',
+        unique=True,
+        db_index=True,
+        validators=[RegexValidator(
+            regex=r'^[\w.@+-]+$',
+            message='Недопустимый символ в имени'
+        )]
+    )
+
+    email = models.EmailField(
+        max_length=254,
+        verbose_name='Адрес электронной почты',
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'{self.username} {self.email}'
