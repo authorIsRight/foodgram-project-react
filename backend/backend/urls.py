36b683e96e7538c8b0e76f1920255aec9cecf770
Илья Соболев <i4sobolev@yandex.ru>
2022-12-12 15:17:56 +0300

    static change

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-# удалить после развертывния в конейтнерах
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
@@ -9,5 +8,7 @@ urlpatterns = [
     path('api/', include('api.urls', namespace='api')),
 ]
 
-# удалить после развертывния в конейтнерах
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
